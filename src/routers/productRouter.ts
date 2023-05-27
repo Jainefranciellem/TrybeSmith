287e795aaf094b107d28b8a49c160ca4aa2c0ba5
Jainefranciellem <jainefraciellen@gmail.com>
2023-05-27 19:03:04 -0300

    created middleware for product

--- src/routers/productRouter.ts
+++ src/routers/productRouter.ts
@@ -1,10 +1,11 @@
 import express from 'express';
 
 import productController from '../controllers/productController';
+import valid from '../middlewares/ProductValidation';
 
 const productRouter = express.Router();
 
-productRouter.post('/', productController.create);
+productRouter.post('/', valid.validateName, valid.validatePrice, productController.create);
 productRouter.get('/', productController.getAll);
 
 export default productRouter;
